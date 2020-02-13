e6d744900b6ec513e67c44dc4e28bd734781d4e3
kmc7468 <kmc7468@naver.com>
2020-02-14 00:57:13 +0900

    Object 클래스 구조 변경
    Type 열거형의 이름을 TypeCode로 변경하고, Type 클래스를 추가하였습니다. Type 클래스는 타입에 대한 정보를 저장하는 클래스입니다.

--- include/svm/Object.hpp
+++ include/svm/Object.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 namespace svm {
-	enum class Type {
+	enum class TypeCode {
 		None,
 
 		Int,
@@ -10,19 +10,39 @@ namespace svm {
 		Reference,
 	};
 
-	class Object {
+	class Type final {
 	public:
-		Object() noexcept = default;
-		Object(const Object& object) noexcept = default;
-		Object(Object&& object) noexcept = default;
-		virtual ~Object() = default;
+		TypeCode Code = TypeCode::None;
+		const char* Name = nullptr;
 
 	public:
-		Object& operator=(const Object& object) noexcept = default;
-		Object& operator=(Object&& object) noexcept = default;
-		
+		Type() noexcept = default;
+		Type(TypeCode code, const char* name) noexcept;
+		Type(const Type&) = delete;
+		~Type() = default;
+
 	public:
-		virtual Type GetType() const noexcept;
+		Type& operator=(const Type&) = delete;
+		bool operator==(const Type&) = delete;
+		bool operator!=(const Type&) = delete;
+	};
+
+	class Object {
+	private:
+		const Type* m_Type = nullptr;
+
+	public:
+		Object() noexcept = default;
+		Object(const Type* type) noexcept;
+		Object(const Object& object) noexcept;
+		~Object() = default;
+
+	public:
+		Object& operator=(const Object& object) noexcept;
+
+	public:
+		const Type* GetType() const noexcept;
+		TypeCode GetTypeCode() const noexcept;
 		bool IsInt() const noexcept;
 		bool IsLong() const noexcept;
 		bool IsDouble() const noexcept;
--- src/Object.cpp
+++ src/Object.cpp
@@ -1,19 +1,37 @@
 #include <svm/Object.hpp>
 
 namespace svm {
-	Type Object::GetType() const noexcept {
-		return Type::None;
+	Type::Type(TypeCode code, const char* name) noexcept
+		: Code(code), Name(name) {}
+}
+
+namespace svm {
+	Object::Object(const Type* type) noexcept
+		: m_Type(type) {}
+	Object::Object(const Object& object) noexcept
+		: m_Type(object.m_Type) {}
+
+	Object& Object::operator=(const Object& object) noexcept {
+		m_Type = object.m_Type;
+		return *this;
+	}
+
+	const Type* Object::GetType() const noexcept {
+		return m_Type;
+	}
+	TypeCode Object::GetTypeCode() const noexcept {
+		return m_Type ? m_Type->Code : TypeCode::None;
 	}
 	bool Object::IsInt() const noexcept {
-		return GetType() == Type::Int;
+		return GetTypeCode() == TypeCode::Int;
 	}
 	bool Object::IsLong() const noexcept {
-		return GetType() == Type::Long;
+		return GetTypeCode() == TypeCode::Long;
 	}
 	bool Object::IsDouble() const noexcept {
-		return GetType() == Type::Double;
+		return GetTypeCode() == TypeCode::Double;
 	}
 	bool Object::IsReference() const noexcept {
-		return GetType() == Type::Reference;
+		return GetTypeCode() == TypeCode::Reference;
 	}
 }
