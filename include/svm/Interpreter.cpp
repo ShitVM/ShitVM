9d682bc83598950f6a617c5211495cb1c271a137
kmc7468 <kmc7468@naver.com>
2020-02-22 22:35:55 +0900

    Stack 클래스 추가
    ShitVM 바이트 파일을 인터프리팅 하면서 지역 변수, 연산 결과 등을 저장하는 스택을 나타내는 클래스입니다.

--- include/svm/Interpreter.cpp
+++ include/svm/Interpreter.cpp
@@ -1,22 +1,73 @@
 #include <svm/Interpreter.hpp>
 
-#include <utility>
+namespace svm {
+	Stack::Stack() noexcept {}
+	Stack::Stack(std::size_t size) {
+		Allocate(size);
+	}
+	Stack::Stack(Stack&& stack) noexcept
+		: m_Data(std::move(stack.m_Data)), m_Size(stack.m_Size), m_Used(stack.m_Used) {}
+
+	Stack& Stack::operator=(Stack&& stack) noexcept {
+		m_Data = std::move(stack.m_Data);
+		m_Size = stack.m_Size;
+		m_Used = stack.m_Used;
+		return *this;
+	}
+
+	void Stack::Allocate(std::size_t size) {
+		m_Data = std::unique_ptr<std::uint8_t[]>(new std::uint8_t[size]());
+		m_Size = size;
+		m_Used = 0;
+	}
+	void Stack::Resize(std::size_t newSize) {
+		if (m_Used > newSize) throw std::runtime_error("Failed to resize the stack. Smaller size than the used size.");
+
+		std::unique_ptr<std::uint8_t[]> newData(new std::uint8_t[newSize]());
+		std::copy(m_Data.get(), m_Data.get() + m_Used, newData.get());
+
+		m_Data = std::move(newData);
+		m_Size = newSize;
+	}
+	void Stack::Deallocate() noexcept {
+		m_Data.reset();
+		m_Size = 0;
+		m_Used = 0;
+	}
+
+	std::size_t Stack::GetSize() const noexcept {
+		return m_Size;
+	}
+	std::size_t Stack::GetUsedSize() const noexcept {
+		return m_Used;
+	}
+}
 
 namespace svm {
 	Interpreter::Interpreter(ByteFile&& byteFile) noexcept
 		: m_ByteFile(std::move(byteFile)) {}
 	Interpreter::Interpreter(Interpreter&& interpreter) noexcept
-		: m_ByteFile(std::move(interpreter.m_ByteFile)) {}
+		: m_ByteFile(std::move(interpreter.m_ByteFile)),
+		m_Stack(std::move(interpreter.m_Stack)) {}
 
 	Interpreter& Interpreter::operator=(Interpreter&& interpreter) noexcept {
 		m_ByteFile = std::move(interpreter.m_ByteFile);
+
+		m_Stack = std::move(interpreter.m_Stack);
 		return *this;
 	}
 
 	void Interpreter::Clear() noexcept {
 		m_ByteFile.Clear();
+		m_Stack.Deallocate();
 	}
 	void Interpreter::Load(ByteFile&& byteFile) noexcept {
 		m_ByteFile = std::move(byteFile);
+	}
+	void Interpreter::AllocateStack(std::size_t size) {
+		m_Stack.Allocate(size);
+	}
+	void Interpreter::ReallocateStack(std::size_t newSize) {
+		m_Stack.Resize(newSize);
 	}
 }
--- include/svm/Interpreter.hpp
+++ include/svm/Interpreter.hpp
@@ -2,10 +2,58 @@
 
 #include <svm/ByteFile.hpp>
 
+#include <cstddef>
+#include <memory>
+#include <utility>
+
 namespace svm {
+	class Stack final {
+	private:
+		std::unique_ptr<std::uint8_t[]> m_Data;
+		std::size_t m_Size = 0;
+		std::size_t m_Used = 0;
+
+	public:
+		Stack() noexcept;
+		explicit Stack(std::size_t size);
+		Stack(Stack&& stack) noexcept;
+		~Stack() = default;
+
+	public:
+		Stack& operator=(Stack&& stack) noexcept;
+
+	public:
+		void Allocate(std::size_t size);
+		void Resize(std::size_t newSize);
+		void Deallocate() noexcept;
+
+		template<typename T>
+		void Push(const T& value) {
+			if (m_Used + sizeof(T) >= m_Size) throw std::runtime_error("Failed to push the value to the stack. Stack overflow.");
+			*reinterpret_cast<T*>(m_Data.get() + m_Used) = value;
+			m_Used += sizeof(T);
+		}
+		template<typename T>
+		T Pop() {
+			if (m_Used < sizeof(T)) throw std::runtime_error("Failed to pop the value from the stack. Not enough space.");
+			T result = *reinterpret_cast<T*>(m_Data.get() + m_Used - sizeof(T));
+			m_Used -= sizeof(T);
+			return std::move(result);
+		}
+		template<typename T>
+		T& Get(std::size_t offset) {
+			if (offset + sizeof(T) < m_Size) throw std::runtime_error("Failed to get the value from the stack. Not enough space.");
+			return *reinterpret_cast<T*>(m_Data.get() + offset);
+		}
+		std::size_t GetSize() const noexcept;
+		std::size_t GetUsedSize() const noexcept;
+	};
+
 	class Interpreter final {
 	private:
 		ByteFile m_ByteFile;
+
+		Stack m_Stack;
 
 	public:
 		Interpreter() noexcept = default;
@@ -21,5 +69,7 @@ namespace svm {
 	public:
 		void Clear() noexcept;
 		void Load(ByteFile&& byteFile) noexcept;
+		void AllocateStack(std::size_t size = 1 * 1024 * 1024);
+		void ReallocateStack(std::size_t newSize);
 	};
 }
