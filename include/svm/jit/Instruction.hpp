4eb4ce72bda8b154a77596cd82e084f5a4dfbdb5
kmc7468 <kmc7468@naver.com>
2020-02-27 17:20:59 +0900

    jit::x86::Instruction 구조체 추가
    x86 명령어 하나를 나타내는 구조체입니다.

---
+++ include/svm/jit/Instruction.hpp
@@ -0,0 +1,45 @@
+#pragma once
+#include <svm/Macro.hpp>
+
+#ifdef SVM_X86
+
+#include <optional>
+
+namespace svm::jit::x86 {
+	union ModRM final {
+		std::uint8_t Byte = 0;
+		struct {
+			std::uint8_t Mod : 2;
+			std::uint8_t Reg : 3;
+			std::uint8_t RM : 3;
+		};
+	};
+
+	union SIB final {
+		std::uint8_t Byte = 0;
+		struct {
+			std::uint8_t Scale : 2;
+			std::uint8_t Index : 3;
+			std::uint8_t Base : 3;
+		};
+	};
+
+	struct Instruction final {
+		std::optional<std::uint8_t> LegacyPrefixGroup1;
+		std::optional<std::uint8_t> LegacyPrefixGroup2;
+		std::optional<std::uint8_t> LegacyPrefixGroup3;
+		std::optional<std::uint8_t> LegacyPrefixGroup4;
+
+		std::optional<std::uint8_t> REXPrefix;
+		std::optional<std::uint8_t> OpCodePrefix1;
+		std::optional<std::uint8_t> OpCodePrefix2;
+		std::optional<std::uint8_t> OpCode;
+		std::optional<x86::ModRM> ModRM;
+		std::optional<x86::SIB> SIB;
+
+		std::optional<std::uint32_t> Displacement;
+		std::optional<std::uint32_t> Immediate;
+	};
+}
+
+#endif
