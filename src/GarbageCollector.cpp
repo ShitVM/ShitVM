9b3c13a9a4997b767a2793bc633805ca94f7845c
kmc7468 <kmc7468@naver.com>
2020-03-15 19:54:44 +0900

    ManagedHeapInfo::Object 멤버 변수 삭제

--- src/GarbageCollector.cpp
+++ src/GarbageCollector.cpp
@@ -3,15 +3,12 @@
 namespace svm {
 	ManagedHeapInfo::ManagedHeapInfo(std::size_t size) noexcept
 		: Size(size) {}
-	ManagedHeapInfo::ManagedHeapInfo(std::size_t size, Type type) noexcept
-		: Size(size), Object(type) {}
 	ManagedHeapInfo::ManagedHeapInfo(const ManagedHeapInfo& info) noexcept
-		: Size(info.Size), Age(info.Age), Object(info.Object) {}
+		: Size(info.Size), Age(info.Age) {}
 
 	ManagedHeapInfo& ManagedHeapInfo::operator=(const ManagedHeapInfo& info) noexcept {
 		Size = info.Size;
 		Age = info.Age;
-		Object = info.Object;
 		return *this;
 	}
 }
--- src/gc/SimpleGarbageCollector.cpp
+++ src/gc/SimpleGarbageCollector.cpp
@@ -1,9 +1,13 @@
 #include <svm/gc/SimpleGarbageCollector.hpp>
 
+#include <cassert>
 #include <cstdlib>
 #include <utility>
 
 namespace svm {
+	SimpleGarbageCollector::SimpleGarbageCollector(std::size_t youngGenerationSize) {
+		Initialize(youngGenerationSize);
+	}
 	SimpleGarbageCollector::SimpleGarbageCollector(SimpleGarbageCollector&& gc) noexcept
 		: m_YoungGenerations(std::move(gc.m_YoungGenerations)), m_Hospital(gc.m_Hospital), m_OldGeneration(std::move(gc.m_OldGeneration)) {}
 	SimpleGarbageCollector::~SimpleGarbageCollector() {
@@ -27,8 +31,47 @@ namespace svm {
 		m_YoungGenerations.clear();
 		m_OldGeneration.clear();
 	}
+	void SimpleGarbageCollector::Initialize(std::size_t youngGenerationSize) {
+		assert(IsEmpty());
+
+		m_YoungGenerations.emplace_back(youngGenerationSize);
+		m_YoungGenerations.emplace_back(youngGenerationSize);
+		m_Hospital = m_YoungGenerations.begin();
+	}
+	bool SimpleGarbageCollector::IsEmpty() const noexcept {
+		return m_YoungGenerations.size() == 0;
+	}
 
 	void* SimpleGarbageCollector::Allocate(std::size_t size) {
-		return nullptr; // TODO
+		size += sizeof(ManagedHeapInfo);
+
+		if (m_Hospital->GetFreeSize() < size) {
+			GC(size);
+		}
+
+		m_Hospital->Expand(size);
+		ManagedHeapInfo* const address = m_Hospital->GetTop<ManagedHeapInfo>();
+		address->Size = size;
+		address->Age = 0;
+
+		return address;
+	}
+
+	void SimpleGarbageCollector::GC(std::size_t size) {
+		// TODO
+	}
+	void SimpleGarbageCollector::MinorGC() {
+
+	}
+	void SimpleGarbageCollector::MajorGC() {
+		// TODO
+	}
+
+	std::list<Stack>::iterator SimpleGarbageCollector::GetNextHosptial() noexcept {
+		auto iter = m_Hospital;
+		++iter;
+
+		if (iter == m_YoungGenerations.end()) return m_YoungGenerations.begin();
+		else return iter;
 	}
 }
