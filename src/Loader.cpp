4b4270bb86284f4303735a50e9bbc0494e5e7c72
static <kmc7468@naver.com>
2023-08-16 20:21:51 +0900

    표준 라이브러리에 파일, 문자열, 정수, 실수 입출력 관련 함수 추가

--- src/Loader.cpp
+++ src/Loader.cpp
@@ -4,13 +4,18 @@
 #include <svm/virtual/VirtualModule.hpp>
 
 #include <algorithm>
+#include <cstddef>
 #include <cstdint>
 #include <cstring>
+#include <filesystem>
 #include <fstream>
 #include <functional>
 #include <iostream>
-#include <utility>
+#include <iterator>
 #include <list>
+#include <utf8.h>
+#include <utility>
+#include <vector>
 
 namespace svm {
 	namespace detail {
@@ -36,7 +41,7 @@ namespace svm::detail::stdlib::string {
 	constexpr std::uint32_t StringLength = 1;
 	constexpr std::uint32_t StringCapacity = 2;
 
-	void Expand32(VirtualContext& context, VirtualObject& string, std::uint64_t required) {
+	void Expand32(VirtualContext& context, const VirtualObject& string, std::uint64_t required) {
 		auto capacity = FIELD(string, StringCapacity).ToLong();
 		if (required <= capacity) return;
 
@@ -59,11 +64,33 @@ namespace svm::detail::stdlib::string {
 		FIELD(string, StringCapacity).SetLong(capacity);
 	}
 
+	std::u32string ConvertToCppString32(VirtualContext& context, const VirtualObject& string) {
+		const auto length = static_cast<std::size_t>(FIELD(string, StringLength).ToLong());
+		const auto data = PDREF(FIELD(string, StringData));
+
+		std::u32string result(length, 0);
+		for (std::size_t i = 0; i < length; ++i) {
+			result[i] = ITEM(data, i).ToInt();
+		}
+		return result;
+	}
+	void ConvertFromCppString32(VirtualContext& context, const std::u32string& cppString, const VirtualObject& string) {
+		const auto size = static_cast<std::uint64_t>(cppString.size());
+		Expand32(context, string, size);
+		FIELD(string, StringLength).SetLong(size);
+
+		const auto data = PDREF(FIELD(string, StringData));
+		for (std::size_t i = 0; i < static_cast<std::size_t>(size); ++i) {
+			ITEM(data, i).SetInt(cppString[i]);
+		}
+	}
+
 	struct State {
 		VirtualModule::StructureIndex VirtualString32;
 
-		void Init(svm::Loader& loader) {
+		void Init(svm::Loader& loader, std::vector<VirtualModule*>& modules) {
 			auto& module = loader.Create("/std/string.sbf");
+			modules.push_back(&module);
 
 			VirtualString32 = module.AddStructure("String32", {
 				{ svm::PointerType, 0 }, // data
@@ -71,7 +98,7 @@ namespace svm::detail::stdlib::string {
 				{ svm::LongType, 0 }, // capacity
 			});
 
-			module.AddFunction("createString", 0, true, [this](VirtualContext& context) {
+			module.AddFunction("create32", 0, true, [this](VirtualContext& context) {
 				auto result = context.PushStructure(STRUCT(VirtualString32));
 				FIELD(result, StringData).SetPointer(VPNULL);
 				FIELD(result, StringLength).SetLong(0);
@@ -122,11 +149,64 @@ namespace svm::detail::stdlib::io {
 
 		virtual ~Stream() = default;
 
+		virtual std::uint32_t ReadInt() = 0;
+		virtual std::uint32_t ReadSignedInt() = 0;
+		virtual std::uint64_t ReadLong() = 0;
+		virtual std::uint64_t ReadSignedLong() = 0;
+		virtual double ReadDouble() = 0;
 		virtual std::uint32_t ReadChar32() = 0;
-		virtual std::int32_t ReadInt() = 0;
+		virtual std::u32string ReadString32() = 0;
 
+		virtual void WriteInt(std::uint32_t value) = 0;
+		virtual void WriteSignedInt(std::uint32_t value) = 0;
+		virtual void WriteLong(std::uint64_t value) = 0;
+		virtual void WriteSignedLong(std::uint64_t value) = 0;
+		virtual void WriteDouble(double value) = 0;
 		virtual void WriteChar32(std::uint32_t value) = 0;
-		virtual void WriteInt(std::int32_t value) = 0;
+		virtual void WriteString32(const std::u32string& value) = 0;
+
+	protected:
+		template<typename T>
+		static std::uint32_t ReadChar32(T& stream) {
+			char utf8[4];
+			stream >> utf8[0];
+
+			if (static_cast<std::uint8_t>(utf8[0]) < 0x80) {
+				return utf8[0];
+			} else if (static_cast<std::uint8_t>(utf8[0]) >= 0xF0) {
+				stream >> utf8[1] >> utf8[2] >> utf8[3];
+				return ((utf8[0] & 0x07) << 18) + ((utf8[1] & 0x3F) << 12) + ((utf8[2] & 0x3F) << 6) + ((utf8[3] & 0x3F) << 0);
+			} else if (static_cast<std::uint8_t>(utf8[0]) >= 0xE0) {
+				stream >> utf8[1] >> utf8[2];
+				return ((utf8[0] & 0x0F) << 12) + ((utf8[1] & 0x3F) << 6) + ((utf8[2] & 0x3F) << 0);
+			} else {
+				stream >> utf8[1];
+				return ((utf8[0] & 0x1F) << 6) + ((utf8[1] & 0x3F) << 0);
+			}
+		}
+		template<typename T>
+		static std::u32string ReadString32(T& stream) {
+			std::string utf8;
+			stream >> utf8;
+
+			std::u32string utf32;
+			utf8::utf8to32(utf8.begin(), utf8.end(), std::back_inserter(utf32));
+			return utf32;
+		}
+
+		template<typename T>
+		static void WriteChar32(T& stream, std::uint32_t value) {
+			const char32_t utf32[2] = { value, 0 };
+			char utf8[5] = {};
+			utf8::utf32to8(std::begin(utf32), std::end(utf32), std::begin(utf8));
+			stream << utf8;
+		}
+		template<typename T>
+		static void WriteString32(T& stream, const std::u32string& value) {
+			std::string utf8;
+			utf8::utf32to8(value.begin(), value.end(), std::back_inserter(utf8));
+			stream << utf8;
+		}
 	};
 
 	struct ReadonlyStream : Stream {
@@ -134,10 +214,25 @@ namespace svm::detail::stdlib::io {
 			IsReadable = true;
 		}
 
+		virtual void WriteInt(std::uint32_t) override {
+			throw std::bad_function_call();
+		}
+		virtual void WriteSignedInt(std::uint32_t) override {
+			throw std::bad_function_call();
+		}
+		virtual void WriteLong(std::uint64_t) override {
+			throw std::bad_function_call();
+		}
+		virtual void WriteSignedLong(std::uint64_t) override {
+			throw std::bad_function_call();
+		}
+		virtual void WriteDouble(double) override {
+			throw std::bad_function_call();
+		}
 		virtual void WriteChar32(std::uint32_t) override {
 			throw std::bad_function_call();
 		}
-		virtual void WriteInt(std::int32_t) override {
+		virtual void WriteString32(const std::u32string&) override {
 			throw std::bad_function_call();
 		}
 	};
@@ -147,33 +242,148 @@ namespace svm::detail::stdlib::io {
 			IsWriteable = true;
 		}
 
+		virtual std::uint32_t ReadInt() override {
+			throw std::bad_function_call();
+		}
+		virtual std::uint32_t ReadSignedInt() override {
+			throw std::bad_function_call();
+		}
+		virtual std::uint64_t ReadLong() override {
+			throw std::bad_function_call();
+		}
+		virtual std::uint64_t ReadSignedLong() override {
+			throw std::bad_function_call();
+		}
+		virtual double ReadDouble() override {
+			throw std::bad_function_call();
+		}
 		virtual std::uint32_t ReadChar32() override {
 			throw std::bad_function_call();
 		}
-		virtual std::int32_t ReadInt() override {
+		virtual std::u32string ReadString32() override {
 			throw std::bad_function_call();
 		}
 	};
 
 	struct StdinStream : ReadonlyStream {
-		virtual std::uint32_t ReadChar32() override {
-			char value;
+		virtual std::uint32_t ReadInt() override {
+			std::uint32_t value;
 			std::cin >> value;
 			return value;
 		}
-		virtual std::int32_t ReadInt() override {
+		virtual std::uint32_t ReadSignedInt() override {
 			std::int32_t value;
 			std::cin >> value;
 			return value;
 		}
+		virtual std::uint64_t ReadLong() override {
+			std::uint64_t value;
+			std::cin >> value;
+			return value;
+		}
+		virtual std::uint64_t ReadSignedLong() override {
+			std::int64_t value;
+			std::cin >> value;
+			return value;
+		}
+		virtual double ReadDouble() override {
+			double value;
+			std::cin >> value;
+			return value;
+		}
+		virtual std::uint32_t ReadChar32() override {
+			return Stream::ReadChar32(std::cin);
+		}
+		virtual std::u32string ReadString32() override {
+			return Stream::ReadString32(std::cin);
+		}
 	};
 
 	struct StdoutStream : WriteonlyStream {
-		virtual void WriteChar32(std::uint32_t value) override {
-			std::cout << static_cast<char>(value);
-		}
-		virtual void WriteInt(std::int32_t value) override {
+		virtual void WriteInt(std::uint32_t value) override {
 			std::cout << value;
+		}
+		virtual void WriteSignedInt(std::uint32_t value) override {
+			std::cout << static_cast<std::int32_t>(value);
+		}
+		virtual void WriteLong(std::uint64_t value) override {
+			std::cout << value;
+		}
+		virtual void WriteSignedLong(std::uint64_t value) override {
+			std::cout << static_cast<std::int64_t>(value);
+		}
+		virtual void WriteDouble(double value) override {
+			std::cout << value;
+		}
+		virtual void WriteChar32(std::uint32_t value) override {
+			Stream::WriteChar32(std::cout, value);
+		}
+		virtual void WriteString32(const std::u32string& value) override {
+			Stream::WriteString32(std::cout, value);
+		}
+	};
+
+	struct FileStream : Stream {
+		std::fstream Stream;
+
+		explicit FileStream(std::fstream stream)
+			: Stream(std::move(stream)) {
+			IsReadable = true;
+			IsWriteable = true;
+		}
+
+		virtual std::uint32_t ReadInt() override {
+			std::uint32_t value;
+			Stream >> value;
+			return value;
+		}
+		virtual std::uint32_t ReadSignedInt() override {
+			std::int32_t value;
+			Stream >> value;
+			return value;
+		}
+		virtual std::uint64_t ReadLong() override {
+			std::uint64_t value;
+			Stream >> value;
+			return value;
+		}
+		virtual std::uint64_t ReadSignedLong() override {
+			std::int64_t value;
+			Stream >> value;
+			return value;
+		}
+		virtual double ReadDouble() override {
+			double value;
+			Stream >> value;
+			return value;
+		}
+		virtual std::uint32_t ReadChar32() override {
+			return Stream::ReadChar32(Stream);
+		}
+		virtual std::u32string ReadString32() override {
+			return Stream::ReadString32(Stream);
+		}
+
+		virtual void WriteInt(std::uint32_t value) override {
+			Stream << value;
+		}
+		virtual void WriteSignedInt(std::uint32_t value) override {
+			Stream << static_cast<std::int32_t>(value);
+		}
+		virtual void WriteLong(std::uint64_t value) override {
+			Stream << value;
+		}
+		virtual void WriteSignedLong(std::uint64_t value) override {
+			Stream << static_cast<std::int64_t>(value);
+		}
+		virtual void WriteDouble(double value) override {
+			Stream << value;
+		}
+		virtual void WriteChar32(std::uint32_t value) override {
+			Stream::WriteChar32(Stream, value);
+		}
+		virtual void WriteString32(const std::u32string& value) override {
+			Stream::WriteString32(Stream, value);
 		}
 	};
 
@@ -216,8 +426,12 @@ namespace svm::detail::stdlib::io {
 
 		VirtualModule::StructureIndex VirtualStream;
 
-		void Init(svm::Loader& loader) {
+		VirtualModule::DependencyIndex StringModule;
+		VirtualModule::MappedStructureIndex VirtualString32;
+
+		void Init(svm::Loader& loader, std::vector<VirtualModule*>& modules) {
 			auto& module = loader.Create("/std/io.sbf");
+			modules.push_back(&module);
 
 			VirtualStream = module.AddStructure("Stream", {
 				{ svm::LongType, 0 }, // _handle
@@ -230,6 +444,111 @@ namespace svm::detail::stdlib::io {
 			module.AddFunction("getStdout", 0, true, [this](VirtualContext& context) {
 				auto result = context.PushStructure(STRUCT(VirtualStream));
 				FIELD(result, 0).SetLong(StreamManager.Stdout);
+			});
+
+			module.AddFunction("openReadonlyFile", 1, true, [this](VirtualContext& context) {
+				auto path = PDREF(PARAM(0)); // TODO: 타입 검사
+				auto cppPath = string::ConvertToCppString32(context, path);
+
+				auto streamHandle = StreamManager.AddStream(std::make_unique<FileStream>(
+					std::fstream(std::filesystem::path(cppPath), std::fstream::in)));
+				auto result = context.PushStructure(STRUCT(VirtualStream));
+				FIELD(result, StreamHandle).SetLong(streamHandle);
+			});
+			module.AddFunction("openWriteonlyFile", 1, true, [this](VirtualContext& context) {
+				auto path = PDREF(PARAM(0)); // TODO: 타입 검사
+				auto cppPath = string::ConvertToCppString32(context, path);
+
+				auto streamHandle = StreamManager.AddStream(std::make_unique<FileStream>(
+					std::fstream(std::filesystem::path(cppPath), std::fstream::out)));
+				auto result = context.PushStructure(STRUCT(VirtualStream));
+				FIELD(result, StreamHandle).SetLong(streamHandle);
+			});
+			module.AddFunction("closeFile", 1, false, [this](VirtualContext& context) {
+				auto stream = PARAM(0); // TODO: 타입 검사
+				auto streamHandle = FIELD(stream, StreamHandle).ToLong();
+				auto& cppStream = static_cast<FileStream&>(StreamManager.GetStream(streamHandle)); // TODO: 유효성 검사
+
+				cppStream.Stream.close();
+				StreamManager.RemoveStream(streamHandle);
+			});
+
+			module.AddFunction("readInt", 1, true, [this](VirtualContext& context) {
+				auto stream = PARAM(0); // TODO: 타입 검사
+				auto streamHandle = FIELD(stream, StreamHandle).ToLong();
+				auto& cppStream = StreamManager.GetStream(streamHandle); // TODO: 유효성 검사
+
+				context.PushFundamental(IntObject(cppStream.ReadInt()));
+			});
+			module.AddFunction("writeInt", 2, false, [this](VirtualContext& context) {
+				auto stream = PARAM(0); // TODO: 타입 검사
+				auto streamHandle = FIELD(stream, StreamHandle).ToLong();
+				auto& cppStream = StreamManager.GetStream(streamHandle); // TODO: 유효성 검사
+
+				auto value = PARAM(1).ToInt(); // TODO: 타입 검사
+				cppStream.WriteInt(value);
+			});
+			module.AddFunction("readSignedInt", 1, true, [this](VirtualContext& context) {
+				auto stream = PARAM(0); // TODO: 타입 검사
+				auto streamHandle = FIELD(stream, StreamHandle).ToLong();
+				auto& cppStream = StreamManager.GetStream(streamHandle); // TODO: 유효성 검사
+
+				context.PushFundamental(IntObject(cppStream.ReadSignedInt()));
+			});
+			module.AddFunction("writeSignedInt", 2, false, [this](VirtualContext& context) {
+				auto stream = PARAM(0); // TODO: 타입 검사
+				auto streamHandle = FIELD(stream, StreamHandle).ToLong();
+				auto& cppStream = StreamManager.GetStream(streamHandle); // TODO: 유효성 검사
+
+				auto value = PARAM(1).ToInt(); // TODO: 타입 검사
+				cppStream.WriteSignedInt(value);
+			});
+
+			module.AddFunction("readLong", 1, true, [this](VirtualContext& context) {
+				auto stream = PARAM(0); // TODO: 타입 검사
+				auto streamHandle = FIELD(stream, StreamHandle).ToLong();
+				auto& cppStream = StreamManager.GetStream(streamHandle); // TODO: 유효성 검사
+
+				context.PushFundamental(LongObject(cppStream.ReadLong()));
+			});
+			module.AddFunction("writeLong", 2, false, [this](VirtualContext& context) {
+				auto stream = PARAM(0); // TODO: 타입 검사
+				auto streamHandle = FIELD(stream, StreamHandle).ToLong();
+				auto& cppStream = StreamManager.GetStream(streamHandle); // TODO: 유효성 검사
+
+				auto value = PARAM(1).ToLong(); // TODO: 타입 검사
+				cppStream.WriteLong(value);
+			});
+			module.AddFunction("readSignedLong", 1, true, [this](VirtualContext& context) {
+				auto stream = PARAM(0); // TODO: 타입 검사
+				auto streamHandle = FIELD(stream, StreamHandle).ToLong();
+				auto& cppStream = StreamManager.GetStream(streamHandle); // TODO: 유효성 검사
+
+				context.PushFundamental(LongObject(cppStream.ReadSignedLong()));
+			});
+			module.AddFunction("writeSignedLong", 2, false, [this](VirtualContext& context) {
+				auto stream = PARAM(0); // TODO: 타입 검사
+				auto streamHandle = FIELD(stream, StreamHandle).ToLong();
+				auto& cppStream = StreamManager.GetStream(streamHandle); // TODO: 유효성 검사
+
+				auto value = PARAM(1).ToLong(); // TODO: 타입 검사
+				cppStream.WriteSignedLong(value);
+			});
+
+			module.AddFunction("readDouble", 1, true, [this](VirtualContext& context) {
+				auto stream = PARAM(0); // TODO: 타입 검사
+				auto streamHandle = FIELD(stream, StreamHandle).ToLong();
+				auto& cppStream = StreamManager.GetStream(streamHandle); // TODO: 유효성 검사
+
+				context.PushFundamental(DoubleObject(cppStream.ReadDouble()));
+			});
+			module.AddFunction("writeDouble", 2, false, [this](VirtualContext& context) {
+				auto stream = PARAM(0); // TODO: 타입 검사
+				auto streamHandle = FIELD(stream, StreamHandle).ToLong();
+				auto& cppStream = StreamManager.GetStream(streamHandle); // TODO: 유효성 검사
+
+				auto value = PARAM(1).ToDouble(); // TODO: 타입 검사
+				cppStream.WriteDouble(value);
 			});
 
 			module.AddFunction("readChar32", 1, true, [this](VirtualContext& context) {
@@ -248,23 +567,25 @@ namespace svm::detail::stdlib::io {
 				cppStream.WriteChar32(value);
 			});
 
-			module.AddFunction("readInt", 1, true, [this](VirtualContext& context) {
+			module.AddFunction("readString32", 1, true, [this](VirtualContext& context) {
 				auto stream = PARAM(0); // TODO: 타입 검사
 				auto streamHandle = FIELD(stream, StreamHandle).ToLong();
 				auto& cppStream = StreamManager.GetStream(streamHandle); // TODO: 유효성 검사
 
-				context.PushFundamental(IntObject(cppStream.ReadInt()));
+				auto result = context.PushStructure(STRUCT(VirtualString32));
+				string::ConvertFromCppString32(context, cppStream.ReadString32(), result);
 			});
-			module.AddFunction("writeInt", 2, false, [this](VirtualContext& context) {
+			module.AddFunction("writeString32", 2, false, [this](VirtualContext& context) {
 				auto stream = PARAM(0); // TODO: 타입 검사
 				auto streamHandle = FIELD(stream, StreamHandle).ToLong();
 				auto& cppStream = StreamManager.GetStream(streamHandle); // TODO: 유효성 검사
 
-				auto value = PARAM(1).ToInt(); // TODO: 타입 검사
-				cppStream.WriteInt(value);
+				auto value = PDREF(PARAM(1)); // TODO: 타입 검사
+				cppStream.WriteString32(string::ConvertToCppString32(context, value));
 			});
 
-			loader.Build(module);
+			StringModule = module.AddDependency("/std/string.sbf");
+			VirtualString32 = module.AddStructureMapping(StringModule, "String32");
 		}
 	};
 }
@@ -279,9 +600,14 @@ namespace svm {
 
 	StdModule InitStdModule(Loader& loader) {
 		StdModule module = std::make_shared<detail::StdModuleState>();
+		
+		std::vector<VirtualModule*> modules;
+		module->IOState.Init(loader, modules);
+		module->StringState.Init(loader, modules);
 
-		module->IOState.Init(loader);
-		module->StringState.Init(loader);
+		for (const auto& module : modules) {
+			loader.Build(*module);
+		}
 
 		return module;
 	}
